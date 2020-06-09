2caa515fce29749533b3d7a0e750b451bafb5d44
Galtigar <43980765+Galtigar@users.noreply.github.com>
2020-06-09 17:31:28 +0530

    Headset Detection PythonFile
    Headset detection using Tensorflow and keras in an Image file

---
+++ headsetDetection.py
@@ -0,0 +1,50 @@
+import tensorflow
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+
+
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+image = Image.open('yes1.jpg')
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+image.show()
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# run the inference
+
+prediction = model.predict(data)
+x,y= prediction[:,[0,1]][0]
+
+#Round the values to the nearest integer
+
+x = round(x)
+
+y = round(y)
+
+#Define the condition
+
+if y == 1 :
+    print("No Headset Detected")
+else:
+    print("Headset Detected")
